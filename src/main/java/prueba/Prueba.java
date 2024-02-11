d46d3420cbd682aab449efbab238a9992adeeb6d
neosbamen <lorzproducciones@gmail.com>
2024-02-11 23:06:13 +0100

    First try

---
+++ src/main/java/prueba/Prueba.java
@@ -0,0 +1,17 @@
+package prueba;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+
+
+        for(int x=1;x<=100;x=x+1){
+            if(x%2==0) {
+                System.out.println(x);
+            }
+        }
+
+
+
+    }
+}
